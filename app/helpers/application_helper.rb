faa35f1a05d938dc0bd52bf6de3cc65de1e120ec
sbannasch <sbannasch@4e27850c-0488-45a1-9bd4-87dd9910adcb>
2006-10-20 21:15:42 +0000

    added application helper image_url and public/images/sail_orangecirc_64.gif
    git-svn-id: https://svn.concord.org/svn/sds/trunk@37 4e27850c-0488-45a1-9bd4-87dd9910adcb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,5 +5,32 @@ module ApplicationHelper
     "<p><b>XML output representation: </b></p>" +
     "<p><code>#{h(obj.to_xml(:except => ['created_at', 'updated_at']))}</code></p>"
   end
+
+  # This method will work the same as image_path
+  # image_path("image.gif") => /images/image.gif
+  # however if there is an optional parameter hash like this:
+  # image_path("image.gif", :only_path => false) => http://rails.host.com/images/image.gif
+  def image_url(path, options=nil)
+    p = image_path(path)
+    if options
+      my_rewrite_url(p, options)
+    else
+      p
+    end
+  end
+
+  # my_rewrite_url grabbed from ActionController::UrlRewriter, file: url_rewriter.rb 
+  def my_rewrite_url(path, options)
+    rewritten_url = ""
+    unless options[:only_path]
+      rewritten_url << (options[:protocol] || @request.protocol)
+      rewritten_url << (options[:host] || @request.host_with_port)
+    end
+#    rewritten_url << @request.relative_url_root.to_s unless options[:skip_relative_url_root]
+    rewritten_url << path
+    rewritten_url << '/' if options[:trailing_slash]
+    rewritten_url << "##{options[:anchor]}" if options[:anchor]
+    rewritten_url
+  end
   
 end
