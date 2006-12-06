ecd6a3206b75b16aa6c1b17c6b5e3976b31cf62d
sbannasch <sbannasch@4e27850c-0488-45a1-9bd4-87dd9910adcb>
2006-12-06 19:42:02 +0000

    update sock filesystem persistence path
    git-svn-id: https://svn.concord.org/svn/sds/trunk@85 4e27850c-0488-45a1-9bd4-87dd9910adcb

--- app/models/sock.rb
+++ app/models/sock.rb
@@ -74,8 +74,8 @@ class Sock < ActiveRecord::Base
   
   def save_to_file_system
     begin
-      File.open("raw/sock_#{self.id.to_s}_#{self.pas_type}_#{self.encoding}", "w") { |f| f.write self.value }
-      File.open("decoded/sock_#{self.id.to_s}_#{self.pas_type}.#{self.extension}", "w") { |f| f.write self.text }
+      File.open("public/socks/raw/sock_#{self.id.to_s}_#{self.pas_type}_#{self.encoding}", "w") { |f| f.write self.value }
+      File.open("public/socks/decoded/sock_#{self.id.to_s}_#{self.pas_type}.#{self.extension}", "w") { |f| f.write self.text }
     end
   end
   
