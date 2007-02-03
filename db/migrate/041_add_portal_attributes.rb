3e8c31ffd5b83bb5368f02d082dc16f32036b117
sbannasch <sbannasch@4e27850c-0488-45a1-9bd4-87dd9910adcb>
2007-02-03 00:06:25 +0000

    addedmigration for new portal parameters
    git-svn-id: https://svn.concord.org/svn/sds/trunk@91 4e27850c-0488-45a1-9bd4-87dd9910adcb

---
+++ db/migrate/041_add_portal_attributes.rb
@@ -0,0 +1,17 @@
+class AddPortalAttributes < ActiveRecord::Migration
+  def self.up
+    add_column :sds_portals, :title, :string
+    add_column :sds_portals, :vendor, :string
+    add_column :sds_portals, :home_page_url, :string
+    add_column :sds_portals, :description, :string
+    add_column :sds_portals, :image_url, :string
+  end
+
+  def self.down
+    remove_column :sds_portals, :title
+    remove_column :sds_portals, :vendor
+    remove_column :sds_portals, :home_page_url
+    remove_column :sds_portals, :description
+    remove_column :sds_portals, :image_url
+  end
+end
