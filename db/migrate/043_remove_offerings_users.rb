69d2e3a4b218f13c93659ce8c4c50b374b8651f1
sbannasch <sbannasch@4e27850c-0488-45a1-9bd4-87dd9910adcb>
2007-03-12 01:12:14 +0000

    updates to the migration tables related to the switch to sail_users
    git-svn-id: https://svn.concord.org/svn/sds/trunk@123 4e27850c-0488-45a1-9bd4-87dd9910adcb

--- db/migrate/043_remove_offerings_users.rb
+++ db/migrate/043_remove_offerings_users.rb
@@ -1,10 +1,10 @@
-class RemoveOfferingsUsers < ActiveRecord::Migration
+class RemoveOfferingsSailUsers < ActiveRecord::Migration
   def self.up
-    drop_table :sds_offerings_users
+    drop_table :sds_offerings_sail_users
   end
 
   def self.down
-    create_table :sds_offerings_users, :id => false do |t|
+    create_table :sds_offerings_sail_users, :id => false do |t|
       t.column :offering_id, :integer, :null => false
       t.column :user_id, :integer, :null => false
     end
