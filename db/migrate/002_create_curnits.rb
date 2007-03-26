4f6d9a10fafcdff08f32a16723f08a89d28e809f
sbannasch <sbannasch@4e27850c-0488-45a1-9bd4-87dd9910adcb>
2007-03-26 20:26:16 +0000

    jnlp and curnit urls now limited to 256 characters instead of 80, sds and sdsdev1 databases changed directly
    git-svn-id: https://svn.concord.org/svn/sds/trunk@155 4e27850c-0488-45a1-9bd4-87dd9910adcb

--- db/migrate/002_create_curnits.rb
+++ db/migrate/002_create_curnits.rb
@@ -3,7 +3,7 @@ class CreateCurnits < ActiveRecord::Migration
     create_table :sds_curnits do |t|
       t.column :portal_id, :integer
       t.column :name, :string, :limit => 60, :null => false
-      t.column :url, :string, :limit => 120, :null => false
+      t.column :url, :string, :limit => 256, :null => false
       t.column :created_at, :timestamp
       t.column :updated_at, :timestamp
     end
--- db/migrate/003_create_jnlps.rb
+++ db/migrate/003_create_jnlps.rb
@@ -3,7 +3,7 @@ class CreateJnlps < ActiveRecord::Migration
     create_table :sds_jnlps do |t|
       t.column :portal_id, :integer
       t.column :name, :string, :limit => 60, :null => false
-      t.column :url, :string, :limit => 120, :null => false
+      t.column :url, :string, :limit => 256, :null => false
       t.column :created_at, :timestamp
       t.column :updated_at, :timestamp
     end
