06ec9781bf23546cae41fa7062839dd04dfbe917
aunger <aunger@4e27850c-0488-45a1-9bd4-87dd9910adcb>
2008-01-31 18:00:37 +0000

    Disabled editing and creating ConfigVersions through the web ui and the REST api. Added a link to the Jnlp show page back to the ConfigVersion show page.
    git-svn-id: https://svn.concord.org/svn/sds/trunk@293 4e27850c-0488-45a1-9bd4-87dd9910adcb

--- app/controllers/config_versions_controller.rb
+++ app/controllers/config_versions_controller.rb
@@ -30,62 +30,65 @@ class ConfigVersionsController < ApplicationController
   # GET /config_versions/new
   # GET /config_versions/new.xml
   def new
-    @config_version = ConfigVersion.new
+    render :text => "You are not allowed to do this", :status => 403
+    # @config_version = ConfigVersion.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @config_version }
-    end
+    # respond_to do |format|
+    #   format.html # new.html.erb
+    #   format.xml  { render :xml => @config_version }
+    # end
   end
 
   # GET /config_versions/1/edit
   def edit
-    @config_version = ConfigVersion.find(params[:id])
+    render :text => "You are not allowed to do this", :status => 403
+    # @config_version = ConfigVersion.find(params[:id])
   end
 
   # POST /config_versions
   # POST /config_versions.xml
   def create
-    logger.info("Params: #{params}")
-    if (request.env['CONTENT_TYPE'] == "application/xml")
-      @config_version = ConfigVersion.new(ConvertXml.xml_to_hash(request.raw_post))
-    else
-      @config_version = ConfigVersion.new(params[:config_version])
-    end
+    render :text => "You are not allowed to do this", :status => 403
+#    if (request.env['CONTENT_TYPE'] == "application/xml")
+#      @config_version = ConfigVersion.new(ConvertXml.xml_to_hash(request.raw_post))
+#    else
+#      @config_version = ConfigVersion.new(params[:config_version])
+#    end
 
-    respond_to do |format|
-      if @config_version.save
-        flash[:notice] = 'ConfigVersion was successfully created.'
-        format.html { redirect_to(@config_version) }
-        format.xml  { render :xml => @config_version, :status => :created, :location => @config_version }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @config_version.errors, :status => :unprocessable_entity }
-      end
-    end
+#    respond_to do |format|
+#      if @config_version.save
+#        flash[:notice] = 'ConfigVersion was successfully created.'
+#        format.html { redirect_to(@config_version) }
+#        format.xml  { render :xml => @config_version, :status => :created, :location => @config_version }
+#      else
+#        format.html { render :action => "new" }
+#        format.xml  { render :xml => @config_version.errors, :status => :unprocessable_entity }
+#      end
+#    end
   end
 
   # PUT /config_versions/1
   # PUT /config_versions/1.xml
   def update
-    @config_version = ConfigVersion.find(params[:id])
+    render :text => "You are not allowed to do this", :status => 403
+#    @config_version = ConfigVersion.find(params[:id])
 
-    respond_to do |format|
-      success = false
-      if (request.env['CONTENT_TYPE'] == "application/xml")
-        success = @config_version.update_attributes(ConvertXml.xml_to_hash(request.raw_post))
-      else
-        success = @config_version.update_attributes(params[:config_version])
-      end
-      if success
-        flash[:notice] = 'ConfigVersion was successfully updated.'
-        format.html { redirect_to(@config_version) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @config_version.errors, :status => :unprocessable_entity }
-      end
-    end
+#    respond_to do |format|
+#      success = false
+#      if (request.env['CONTENT_TYPE'] == "application/xml")
+#        success = @config_version.update_attributes(ConvertXml.xml_to_hash(request.raw_post))
+#      else
+#        success = @config_version.update_attributes(params[:config_version])
+#      end
+#      if success
+#        flash[:notice] = 'ConfigVersion was successfully updated.'
+#        format.html { redirect_to(@config_version) }
+#        format.xml  { head :ok }
+#      else
+#        format.html { render :action => "edit" }
+#        format.xml  { render :xml => @config_version.errors, :status => :unprocessable_entity }
+#      end
+#    end
   end
 
   # DELETE /config_versions/1
--- app/models/config_version.rb
+++ app/models/config_version.rb
@@ -2,4 +2,17 @@ class ConfigVersion < ActiveRecord::Base
   set_table_name "sds_config_versions"
   
   has_many :jnlp
+  
+  before_save :verify_valid_template
+  
+  def verify_valid_template
+    begin
+      # TODO how do we verify we have a valid template?
+      # most templates will rely on externally set variables, which aren't going to be set here
+      # so a straight eval won't work. Is there a way to parse it just to check if it's well-formed?
+      # result = eval(self.template)
+    rescue Exception => e
+      raise "Invalid template #{e}"
+    end
+  end
 end
