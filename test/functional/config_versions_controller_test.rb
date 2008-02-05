85f59b7bbb0576819cf51447bbe73ed8d7e27fcb
aunger <aunger@4e27850c-0488-45a1-9bd4-87dd9910adcb>
2008-02-05 21:00:59 +0000

    Misc testing framework for ConfigVersions and LogBundles. There aren't any tests here, but I figured I'll check in the framework in case we write some tests eventually.
    git-svn-id: https://svn.concord.org/svn/sds/trunk@310 4e27850c-0488-45a1-9bd4-87dd9910adcb

---
+++ test/functional/config_versions_controller_test.rb
@@ -0,0 +1,8 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ConfigVersionsControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
