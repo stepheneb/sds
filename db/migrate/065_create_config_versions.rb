250dd9fdd6d4e2395188357b7b4c1294161a3eb5
aunger <aunger@4e27850c-0488-45a1-9bd4-87dd9910adcb>
2008-02-12 18:21:32 +0000

    Added a warning to db migration 65 (the one that sets up the ConfigVersions).
    git-svn-id: https://svn.concord.org/svn/sds/trunk@315 4e27850c-0488-45a1-9bd4-87dd9910adcb

--- db/migrate/065_create_config_versions.rb
+++ db/migrate/065_create_config_versions.rb
@@ -6,6 +6,8 @@ class CreateConfigVersions < ActiveRecord::Migration
       t.column :template, :text
       t.timestamps
     end
+    
+    puts "\nBE SURE TO RUN rake sds:setup_config_versions !!!\n"
   end
 
   def self.down
