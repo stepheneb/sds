ca7578718712406fb4344ae8fc4ee9e28f915cab
aunger <aunger@4e27850c-0488-45a1-9bd4-87dd9910adcb>
2009-01-23 19:11:29 +0000

    Merged the observers initializer into the sds initializer, in order to guarantee load order.
    git-svn-id: https://svn.concord.org/svn/sds/trunk@496 4e27850c-0488-45a1-9bd4-87dd9910adcb

--- config/initializers/observers.rb
+++
@@ -1,8 +0,0 @@
-# :user_observer triggers activation emails to be sent when a user registers, and confirmation email to be sent when they activate
-# :bundle_observer triggers notifications to be sent whenever a bundle is created
-
-ActiveRecord::Base.observers << :user_observer
-ActiveRecord::Base.observers << :bundle_observer
-
-# have to call instantiate_observers since at the point that initializers are run, the observers have already been instantiated
-ActiveRecord::Base.instantiate_observers
